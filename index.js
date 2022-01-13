557d3d0a8e3445040fbb095e87af55a65ff8339b
Mendy <mendyw94@gmail.com>
2022-01-12 22:59:46 -0500

    commit tree b11df9a7802251dc562aae697814d8609dc41836

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching (arrayName, stringName) {
+    let results = [];
+    arrayName.map(function (x) {
+        if(x.toUpperCase() === stringName.toUpperCase()) {
+            results.push(x);
+        }
+    })
+    return results;
+    
+};
+
+function fuzzyMatch (arrayName, stringName) {
+    // let letter = stringName;
+    return arrayName.filter(x => x[0] === stringName[0]);
+};
+
+function matchName (arrayName, stringName) {
+    return arrayName.filter(x => x.name === stringName);
+}
